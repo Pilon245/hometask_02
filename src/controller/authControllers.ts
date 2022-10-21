17728b8aa4bcd0e48a9af1919b537bb61b2a516d
Amirhanov Said <saidderbentskij@yandex.ru>
2022-10-21 22:10:59 +0300

    9.05 secure hometask_9 commit

--- src/controller/authControllers.ts
+++ src/controller/authControllers.ts
@@ -19,7 +19,6 @@ export const authControllers = {
             const accessToken = await jwtService.createdJWT(user)
             const refreshToken = await jwtService.createdRefreshJWT(user,String(uuidv4()))
             await sessionService.createSession(user, req.ip, req.headers['user-agent']!,refreshToken)
-            // await usersRepository.createToken(user.id, accessToken, refreshToken) //todo  через  сервис нужно делать?
             const result = {accessToken: accessToken}
             return res.status(200).cookie("refreshToken", refreshToken,
                 {expires: new Date(Date.now()+ 20000), httpOnly: true, secure: true})
