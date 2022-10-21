dd9a7d788a3edf338f6ca7a389ea9be5d68d637a
Amirhanov Said <saidderbentskij@yandex.ru>
2022-10-21 21:55:20 +0300

    9.04 get/device hometask_9 commit

--- src/repositories/sessionRepository.ts
+++ src/repositories/sessionRepository.ts
@@ -4,7 +4,7 @@ import {SessionDBType, SessionType} from "../types/sessionTypes";
 export const sessionRepository = {
     async findDevices(userId: string): Promise<SessionType []> {
         const result =  await sessionCollection.find({userId: userId})
-            .project({_id:0, userId: 0, expiresDate: 0, ip: 0, title: 0, lastActiveDate: 0})
+            .project({_id:0, userId: 0, expiresDate: 0})
             .toArray()
         return result as SessionType []
     },
